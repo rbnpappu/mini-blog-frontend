2d8662477d83cd183b2c5be568b030b58283fd41
rbnpappu <thakurpappu905@gmail.com>
2025-06-27 10:17:52 +0530

    second commit

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,18 +1,13 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSelector } from 'react-redux';
+import { useParams, useRouter } from 'next/navigation';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@/app/redux/store';
-import Loading from '@/app/components/loading';
-import { useDispatch } from 'react-redux';
 import { clearBlogPost } from '@/app/redux/blogSlice';
 
-interface PostPageProps {
-  params: { id: string };
-}
-
-const PostPage = ({ params }: PostPageProps) => {
+const PostPage = () => {
+  const { id } = useParams();
   const router = useRouter();
   const [isMobile, setIsMobile] = useState(false);
   const dispatch = useDispatch();
@@ -25,37 +20,19 @@ const PostPage = ({ params }: PostPageProps) => {
   };
 
   useEffect(() => {
-    const checkMobile = () => {
-      setIsMobile(window.innerWidth <= 765);
-    };
+    const checkMobile = () => setIsMobile(window.innerWidth <= 765);
     checkMobile();
     window.addEventListener('resize', checkMobile);
     return () => window.removeEventListener('resize', checkMobile);
   }, []);
 
   return (
-    <div
-      className = 'h-screen bg-[#08f1e6] relative w-full p-6 overflow-auto'
-    >
-      <div
-        className="flex flex-col justify-between items-center bg-[#fff] p-8 rounded-[1rem] gap-6"
-        style={{
-          boxShadow:
-            'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
-        }}
-      >
-        <h3
-          className={`text-left font-sans font-[900] text-[#3e3e3e] ${
-            isMobile ? 'text-[2rem]' : 'text-[3rem]'
-          }`}
-        >
+    <div className='h-screen bg-[#08f1e6] relative w-full p-6 overflow-auto'>
+      <div className="flex flex-col justify-between items-center bg-[#fff] p-8 rounded-[1rem] gap-6" style={{ boxShadow: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px' }}>
+        <h3 className={`text-left font-sans font-[900] text-[#3e3e3e] ${isMobile ? 'text-[2rem]' : 'text-[3rem]'}`}>
           {post.title.charAt(0).toUpperCase() + post.title.slice(1)}
         </h3>
-        <p
-          className={`text-left font-sans font-[400] text-[#3e3e3e] ${
-            isMobile ? 'text-[2.6rem]' : 'text-[2rem]'
-          }`}
-        >
+        <p className={`text-left font-sans font-[400] text-[#3e3e3e] ${isMobile ? 'text-[2.6rem]' : 'text-[2rem]'}`}>
           {post.body}
         </p>
         <button
